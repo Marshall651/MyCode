5bf69f3e543bacb34a018694476995d5e3bc7c6e
wuhaobin <429027214@qq.com>
2022-09-01 15:54:53 +0800

    父模块

---
+++ SpringBoot/src/main/java/com/rushuni/lifengsmarthealth/LifengSmartHealthApplication.java
@@ -0,0 +1,13 @@
+package com.rushuni.lifengsmarthealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifengSmartHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifengSmartHealthApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/src/test/java/com/rushuni/lifengsmarthealth/LifengSmartHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushuni.lifengsmarthealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifengSmartHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
